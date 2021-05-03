261d1a1347a29d024f93e08ddb27b4a904a1dca9
galap <galkowski.p97@gmail.com>
2021-05-03 16:52:19 +0200

    contentful props

---
+++ pages/recipes/[slug].jsx
@@ -0,0 +1,98 @@
+import { createClient }  from 'contentful'
+import Image from 'next/image'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'recipe'
+  })
+
+  const paths = res.items.map(item => {
+    return {
+      params: {
+        slug: item.fields.slug
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug
+  })
+  return {
+    props: {
+      recipe: items[0]
+    }
+  }
+}
+
+
+export default function RecipeDetails({ recipe }) {
+  const { featuredImage, title, cookingTime, ingredients, method} = recipe.fields;
+  
+  return (
+    <div>
+      <div className="banner">
+        <Image 
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+          src={`https:${featuredImage.fields.file.url}`}/>
+          <h2>{ title }</h2>
+      </div>
+      <div className="info">
+        <p>
+          Takes about { cookingTime } mins to prepare 
+        </p>
+        <h3>
+          Ingredients:
+        </h3>
+
+          <ul>
+            {
+              ingredients.map(ingredient => (
+                  <li key={ingredient}> { ingredient } </li>
+              ))
+            }
+        </ul>
+      </div>
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{ documentToReactComponents(method) }</div>
+      </div>
+
+
+      <style jsx>{`
+                h2,h3 {
+                  text-transform: uppercase;
+                }
+                .banner h2 {
+                  margin: 0;
+                  background: #fff;
+                  display: inline-block;
+                  padding: 20px;
+                  position: relative;
+                  top: -60px;
+                  left: -10px;
+                  transform: rotateZ(-1deg);
+                  box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+                }
+                .info p {
+                  margin: 0;
+                }
+      `}</style>
+    </div>
+  )
+}
